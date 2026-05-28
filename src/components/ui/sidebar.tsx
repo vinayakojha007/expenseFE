cc5b18311ce14f111e54d226ebbd637567f56b9d
vinayakojha007 <vinayak.ojha@kelltontech.com>
2026-05-28 22:52:12 +0530

    removed unused imports

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -2,7 +2,6 @@
 
 import * as React from "react"
 import { cva, type VariantProps } from "class-variance-authority"
-import { Slot } from "@radix-ui/react-slot"
 import { PanelLeftIcon } from "lucide-react"
 
 import { useIsMobile } from "@/hooks/use-mobile"
